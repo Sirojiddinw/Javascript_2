1fd7282d5296ab20efa86ceb928097e10b2e5a3c
Sirojiddin <Sirojiddinyusupov55@gmail.com>
2024-03-07 20:00:43 +0500

    JS2

---
+++ script.js
@@ -0,0 +1,54 @@
+var name = prompt(" Ismingizni kiriting ")
+console.log(" Sizning ismingiz "+name);
+
+
+var age = prompt(" Yoshingizni kiriting ")
+console.log(" Szning yoshingiz "+age);
+
+
+var num = +prompt(" 250+250=? ")
+if (num>=500) {
+    console.log(" Szning javobingiz togri 500 ");
+}else{
+    console.log("Szning javobingiz natogri togri javob 500");
+}
+
+
+var num = +prompt(" 150-50=? ")
+if (num>=100) {
+    console.log(" Szning javobingiz togri 100 ");
+}else{
+    console.log(" Szning javobingiz notogri togri javob 100");
+}
+
+
+var num = +prompt(" 5*5=? ")
+if (num>=25) {
+    console.log(" Szning javobingiz togri 25 ");
+}else{
+    console.log("Szning javobingiz natogri togri javob 25");
+}
+
+
+var num = +prompt(" 500*2 ")
+if (num>=1000) {
+    console.log("Szning javobingiz togri 1000");
+}else{
+    console.log(" Szning javobingiz natogri togri javob 1000 ");
+}
+
+
+var num = +prompt(" 100/2 ")
+if (num>=50) {
+    console.log(" Szning javobingiz togri 50 ");
+}else{
+    console.log(" Szning javobingiz natogri togri javob 50 ");
+}
+
+
+var num = +prompt("180/2")
+if (num>=140) {
+    console.log(" Szning javobingiztogri 140 ");
+}else{
+    console.log(" Szning javobingiz natogri togri javob 140 ");
+}
